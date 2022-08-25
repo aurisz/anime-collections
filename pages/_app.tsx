664a4209d99fc3d908615bbb4a4b82705bf53ee4
Auri <aurisophanz@gmail.com>
2022-08-25 22:29:29 +0700

    feat(apollo): setup apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
 import type { AppProps } from 'next/app'
 
+const client = new ApolloClient({
+  uri: 'https://graphql.anilist.co',
+  cache: new InMemoryCache(),
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
